1933b97f9a803ddbe726d24fa6b0aa77c32d8e38
Oconcejero <oconcejero@gmail.com>
2026-02-23 15:31:59 +0100

    Añadiendo Base de datos

--- a_crm_django/settings.py
+++ a_crm_django/settings.py
@@ -1,6 +1,8 @@
 
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +75,13 @@ WSGI_APPLICATION = 'a_crm_django.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
