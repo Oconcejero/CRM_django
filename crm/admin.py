a2bfec78fe9bb2e182fea10cefb5fe1e4f1ac2d8
Oconcejero <oconcejero@gmail.com>
2026-02-24 11:16:46 +0100

    Prueba admin 02

--- crm/admin.py
+++ crm/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
-admin.site.unregister(User)
-admin.site.unregister(Group)
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+try:
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass
