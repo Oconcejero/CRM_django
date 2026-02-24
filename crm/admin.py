9dbde97dd79fe177087992a6344b9df8b3d96e60
Oconcejero <oconcejero@gmail.com>
2026-02-24 11:21:19 +0100

    Cambio nuevo en admin 003

--- crm/admin.py
+++ crm/admin.py
@@ -1,12 +1,1 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
-
-try:
-    admin.site.unregister(User)
-except admin.sites.NotRegistered:
-    pass
-
-try:
-    admin.site.unregister(Group)
-except admin.sites.NotRegistered:
-    pass
